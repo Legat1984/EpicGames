1583b8f6b83899d4c5faead5889628639c1a7976
Rudnik Sergey (Legat) <indeo_1984@mail.ru>
2025-11-27 13:47:46 +0300

    Работа с всплывающими подсказками 3

--- src/components/Platform/components/header/FavoriteGame.js
+++ src/components/Platform/components/header/FavoriteGame.js
@@ -1,30 +1,5 @@
 import styled from 'styled-components';
 
-const Tooltip = styled.div`
-  position: absolute;
-  bottom: -30px;
-  left: 50%;
-  transform: translateX(-50%);
-  background-color: ${props => props.theme.surface};
-  color: ${props => props.theme.text};
-  border: 1px solid ${props => props.theme.border};
-  border-radius: 4px;
-  padding: 0.25rem 0.5rem;
-  font-size: 0.75rem;
-  white-space: nowrap;
-  z-index: 1000;
-  opacity: 0;
-  display: none;
-  transition: opacity 0.2s;
-  box-shadow: 0 2px 8px rgba(0,0,0,0.15);
-  pointer-events: none;
-
-  ${props => props.visible && `
-    opacity: 1;
-    display: block;
-  `}
-`;
-
 const StyledFavoriteGame = styled.div`
   position: relative;
   display: flex;
@@ -34,13 +9,6 @@ const StyledFavoriteGame = styled.div`
   min-width: 60px;
   cursor: pointer;
   overflow: visible;
-
-  &:hover ${Tooltip} {
-    @media (min-width: 769px) {
-      display: block;
-      opacity: 1;
-    }
-  }
   
   img {
     width: 40px;
@@ -53,8 +21,7 @@ const StyledFavoriteGame = styled.div`
 const FavoriteGame = ({ game, theme }) => {
   return (
     <StyledFavoriteGame theme={theme}>
-      <img src={game.image} alt={game.title} />
-      <Tooltip theme={theme}>{game.title}</Tooltip>
+      <img src={game.image} alt={game.title} title={game.title} />
     </StyledFavoriteGame>
   );
 };
